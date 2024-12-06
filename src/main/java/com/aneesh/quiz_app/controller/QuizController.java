c92a586a081021495b2c46093b8b34e4266c8a47
Aneesh Tripathi <85986613+Aneesh-07@users.noreply.github.com>
2024-12-06 16:44:39 +0530

    Add files via upload

---
+++ src/main/java/com/aneesh/quiz_app/controller/QuizController.java
@@ -0,0 +1,65 @@
+package com.aneesh.quiz_app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import com.aneesh.quiz_app.model.Question;
+import com.aneesh.quiz_app.model.QuestionWrapper;
+import com.aneesh.quiz_app.model.Response;
+import com.aneesh.quiz_app.service.QuizService;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category , @RequestParam int numQ, @RequestParam String title){
+
+
+        return quizService.createQuiz(category,numQ,title);
+    }
+
+
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+
+        return quizService.getQuizQuestions(id);
+
+
+    }
+    
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+
+
+
+        return quizService.calculateResult(id,responses);
+
+        
+    
+    
+        }
+
+
+
+
+
+
+    }
+
+
